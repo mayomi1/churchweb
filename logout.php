654b45dc5be0aeb32575168c00fb870429cd5426
Ayandiran Mayomi <ayandiranmayomi@gmail.com>
2016-08-22 01:53:23 +0100

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * codes written by Ayandiran Mayomi
+ * 07036725156
+ * ayandiranmayomi@gmail.com
+ *instagram ::: @mayomi1
+ */
+session_start();
+session_unset();
+session_destroy();
+header("Location: Jesusislord.php");
+
+?>
